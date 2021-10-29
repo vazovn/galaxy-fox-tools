e3efba82c7f267c6d00a7b6a8289cd1fdfeeef0e
Sabry Razick <sabryr@gmail.com>
2021-10-29 18:27:54 +0200

    bam to sam2

--- tool_dependencies/get_job_status/1.0.0/bin/g_load.sh
+++ tool_dependencies/get_job_status/1.0.0/bin/g_load.sh
@@ -3,4 +3,8 @@
 export PATH=/cluster/bin:$PATH
 export PERL5LIB=/node/lib/perl5:/cluster/lib/perl5
 
+echo '<html<title>SAGA load</title><body>  src="https://metadoc.metacenter.no/status_graph/?machine=saga&period=week&type=cpu&size=large&dynamic=true&start=0&end=0&format=png" alt="https://metadoc.metacenter.no/status_graph/?machine=saga&period=week&type=cpu&size=large&dynamic=true&start=0&end=0&format=png"></body></html>' > out.html
 freepe
+#echo '</body></html>'i
+
+
