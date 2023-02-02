8c7aaaafc07e912a142832ddff45ca498bf6b930
vazovn <n.a.vazov@usit.uio.no>
2023-02-02 13:00:31 +0100

    Edit Trinity verison

--- tool_dependencies/trinity/2.10.0/env.sh
+++ tool_dependencies/trinity/2.10.0/env.sh
@@ -4,4 +4,5 @@ if [ -z "$MODULEPATH" ] ; then
    . /etc/profile.d/z01_StdEnv.sh
 fi
 
-module load Trinity/2.10.0-foss-2020a-Python-3.8.2
+#module load Trinity/2.10.0-foss-2020a-Python-3.8.2
+module add Trinity/2.10.0-foss-2019b-Python-3.7.4
